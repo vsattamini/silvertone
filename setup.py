da4e7f0e8a69710e248ff0add8d16f60c381abbb
Victor Sattamini <victor.l.sattamini@gmail.com>
2022-11-21 12:08:50 -0300

    fixing virtual env

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='silvertone',
-      version="1.0",
-      description="Project Description",
-      packages=find_packages(),
-      install_requires=requirements,
-      test_suite='tests',
-      # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      scripts=['scripts/silvertone-run'],
-      zip_safe=False)
+# setup(name='silvertone',
+#       version="1.0",
+#       description="Project Description",
+#       packages=find_packages(),
+#       install_requires=requirements,
+#       test_suite='tests',
+#       # include_package_data: to install data from MANIFEST.in
+#       include_package_data=True,
+#       scripts=['scripts/silvertone-run'],
+#       zip_safe=False)
